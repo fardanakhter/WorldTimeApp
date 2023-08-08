a8f4e77dbaee8a0c4b2420eae949417911558bc2
Fardan Akhter <fardan_akhter@hotmail.com>
2023-08-08 17:29:18 +0500

    Move async code with network request to loading view

--- world_time/lib/pages/loading.dart
+++ world_time/lib/pages/loading.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -8,6 +10,21 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  getData() async {
+    var url = Uri.http('jsonplaceholder.typicode.com', '/todos/1');
+    Response response = await get(url);
+    Map map = jsonDecode(response.body);
+    print(map['id']);
+    print(map['title']);
+    print(map['completed']);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Text("Loading Screen");
