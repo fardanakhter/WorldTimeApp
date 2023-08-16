cf610531f3de8fb2d9e10901729f7cb1a7c0726b
Fardan Akhter <fardan_akhter@hotmail.com>
2023-08-16 22:57:08 +0500

    Add loading indicator on home screen when location request is awaiting

--- world_time/lib/pages/home.dart
+++ world_time/lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -9,6 +10,7 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   Map data = {};
+  bool showLoadingIndicator = false;
 
   @override
   Widget build(BuildContext context) {
@@ -21,55 +23,78 @@ class _HomeState extends State<Home> {
 
     return Scaffold(
         backgroundColor: Colors.blue[400],
-        body: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("assets/$backgroundImage"),
-                fit: BoxFit.cover),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextButton.icon(
-                  style: TextButton.styleFrom(foregroundColor: Colors.white),
-                  onPressed: () async {
-                    dynamic data =
-                        await Navigator.pushNamed(context, "/location");
-                    setState(() {
-                      this.data = data;
-                    });
-                  },
-                  icon: const Icon(Icons.edit_location),
-                  label: const Text("edit location")),
-              const SizedBox(height: 18),
-              Row(
+        body: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/$backgroundImage"),
+                    fit: BoxFit.cover),
+              ),
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text(
-                    data['location'],
-                    style: const TextStyle(
-                        fontSize: 32.0,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w300,
-                        letterSpacing: 2.0),
+                  TextButton.icon(
+                      style:
+                          TextButton.styleFrom(foregroundColor: Colors.white),
+                      onPressed: () async {
+                        // show loading indicator
+                        setState(() {
+                          showLoadingIndicator = true;
+                        });
+
+                        // Wait for edit location request completion
+                        dynamic data =
+                            await Navigator.pushNamed(context, "/location");
+
+                        // Wait for few secs to stop loading indicator
+                        // await Future.delayed(const Duration(seconds: 1));
+                        setState(() {
+                          showLoadingIndicator = false;
+                          this.data = data;
+                        });
+                      },
+                      icon: const Icon(Icons.edit_location),
+                      label: const Text("edit location")),
+                  const SizedBox(height: 18),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        data['location'],
+                        style: const TextStyle(
+                            fontSize: 32.0,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w300,
+                            letterSpacing: 2.0),
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 22),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        data['time'],
+                        style: const TextStyle(
+                            fontSize: 66.0,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w300),
+                      )
+                    ],
                   )
                 ],
               ),
-              const SizedBox(height: 22),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    data['time'],
-                    style: const TextStyle(
-                        fontSize: 66.0,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w300),
-                  )
-                ],
-              )
-            ],
-          ),
+            ),
+            Visibility(
+              visible: showLoadingIndicator,
+              child: Container(
+                color: Colors.black38,
+                child: const Center(
+                    child: SpinKitFadingCube(color: Colors.white, size: 50.0)),
+              ),
+            )
+          ],
         ));
   }
 }
